76ddb1d2e9518753abe69b2d22c40709e31f25eb
Tomek Szpinda <tszpinda@gmail.com>
2021-11-05 15:09:57 +0000

    Widget with background depending on dark/light mode

---
+++ app/src/main/java/com/example/widget/Widget.kt
@@ -0,0 +1,33 @@
+package com.example.widget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.os.Build
+import android.widget.RemoteViews
+import androidx.annotation.RequiresApi
+
+class Widget: AppWidgetProvider() {
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onUpdate(
+        context: Context?,
+        appWidgetManager: AppWidgetManager?,
+        appWidgetIds: IntArray?
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+        if (appWidgetIds == null) return;
+        if (appWidgetManager == null) return;
+        if (context == null) return;
+
+        val viewMapping = RemoteViews(
+            context.packageName,
+            R.layout.widget_medium
+        );
+
+        for (appWidgetId in appWidgetIds) {
+            appWidgetManager.updateAppWidget(appWidgetId, RemoteViews(viewMapping))
+        }
+    }
+
+}
